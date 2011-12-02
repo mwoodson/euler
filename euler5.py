70ccb0e0df109c770a321194f74b0a7fd7c63a92
Kenny Woodson <kwoodson@redhat.com>
2011-12-02 13:34:51 -0500

    fixed euler5

--- euler5.py
+++ euler5.py
@@ -5,12 +5,12 @@
 
 What is the smallest number that is evenly divisible by all of the numbers from 1 to 20?
 '''
+nums = [19,13,18,17,16,15,14,13,12,11]
 
 found = False
-for i in xrange(2520, 1000000000, 20):
-    for x in xrange(2, 21):
-        if i % x != 0:
-            break
+for i in xrange(2520, 1000000000, 2520):
+    if any(z for z in nums if i % z != 0):
+        continue
     else:
         print i
         break
