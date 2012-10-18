6eb552124914df14decddbe72485c0fb613ac203
Kenny Woodson <kwoodson@redhat.com>
2012-10-18 16:10:14 -0400

    solved but could use some improvement

--- euler39.py
+++ euler39.py
@@ -11,15 +11,21 @@ For which value of p<= 1000 is the number of solutions maximized?
 
 
 def main():
-    for p in range(1,1001):
-        for x in range(1,p):
-            for y in range(x,p):
-                for z in range(y,p):
-                    if x**2 + y**2 == z:
-                        print x,y,z,p
+    counter = {}
+    largest = (0,0)
+    for p in range(4,1001,2):
+        counter[p] = 0
+        for a in range(3, p/4 + 1):
+            for b in range(a+1,(p-a)/2):
+                z = a**2 + b**2
+                c = z**.5
+                if a + b + c == p:
+                    counter[p] += 1
+        if counter[p] > largest[1]:
+            largest = (p,counter[p])
 
 
-
+    print largest
 
 
 if __name__ == "__main__":
