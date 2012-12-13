a89717a411daba8a41fea81d11219ca6cda51805
Kenny Woodson <kwoodson@redhat.com>
2012-12-12 22:17:38 -0500

    58

---
+++ ruby_euler/reuler58a.rb
@@ -0,0 +1,63 @@
+#!/usr/bin/env ruby
+
+require 'pp'
+require 'mathn'
+
+def main
+
+    
+    primes = Prime.instance
+    moves = 2
+    diag = 1
+    c = 1
+    pc = 0
+    z = 100
+    while z > 10 && c > 0
+        #see if our percentage is met
+        0.upto(3).each do |ind| 
+            c += moves
+            pc += 1 if primes.prime?(c)
+            diag += 1
+        end
+        moves += 2
+
+        z = (pc / diag).to_f * 100
+    end
+    puts "Iteration: #{c **0.5}: c=#{c} z=#{z}"
+end
+
+if __FILE__ == $0
+    main
+end
+
+
+__END__
+
+Starting with 1 and spiralling anticlockwise in the following way, a square spiral with side length 7 is formed.
+
+37 36 35 34 33 32 31
+38 17 16 15 14 13 30
+39 18  5  4  3 12 29
+40 19  6  1  2 11 28
+41 20  7  8  9 10 27
+42 21 22 23 24 25 26
+43 44 45 46 47 48 49
+
+It is interesting to note that the odd squares lie along the bottom right diagonal, but what is more interesting is that 8 out of the 13 numbers lying along both diagonals are prime; that is, a ratio of 8/13  62%.
+
+If one complete new layer is wrapped around the spiral above, a square spiral with side length 9 will be formed. If this process is continued, what is the side length of the square spiral for which the ratio of primes along both diagonals first falls below 10%?
+
+
+
+=begin
+#go right
+1 1 1
+#go up
+1 d 3 5
+#go left
+2 d 4 6
+#go down
+2 d 4 6
+#go right
+2 d 4 6
+=end
