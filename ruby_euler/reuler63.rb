e8d76e0b157313b71e5b8f19520c6131ce601f73
Kenny Woodson <kwoodson@redhat.com>
2013-09-06 15:49:04 -0400

    63 down

--- ruby_euler/reuler63.rb
+++ ruby_euler/reuler63.rb
@@ -11,7 +11,25 @@ How many n-digit positive integers exist which are also an nth power?
 =end
 
 def main
+    a = '1' + '0' * 9
+    puts a.to_i
+    a = '20000'
+    results = []
+    1.upto(21).each do |pow|
+        1.upto(99).each do |x|
+            result = x ** pow
+            length = result.to_s.size
+#puts "length=>#{length} result=>#{result} #{pow}" if x == 7
+            if length == pow
+                puts "x(#{x}) pow(#{pow}) => length(#{length}) result(#{result})"
+                results << result
+            elsif length > pow
+                break
+            end
+        end
+    end
     
+    puts results.length
 end
 
 if __FILE__ == $0
