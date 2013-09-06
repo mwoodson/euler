39fcdaed9bd75b6efb5e184170fad00d98360573
Kenny Woodson <kwoodson@redhat.com>
2013-09-06 15:25:34 -0400

    adding 63

---
+++ ruby_euler/reuler63.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+require 'pp'
+#relative_require 'lib/helper'
+require_relative 'lib/helper'
+
+=begin
+The 5-digit number, 16807=7^5, is also a fifth power. Similarly, the 9-digit number, 134217728=8^9, is a ninth power.
+
+How many n-digit positive integers exist which are also an nth power?
+=end
+
+def main
+    
+end
+
+if __FILE__ == $0
+    main
+end
