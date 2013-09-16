0869d37c0bd1d7e8c5f05d70e1435c0d317e49ee
Kenny Woodson <kwoodson@redhat.com>
2013-09-16 13:45:38 -0400

    Finished

--- ruby_euler/euler68/reuler68.rb
+++ ruby_euler/euler68/reuler68.rb
@@ -52,15 +52,18 @@ def main
         perms.each do |p|
             result = num + p.inject(:+)
             results[num][result] ||= []
-            results[num][result] << p.sort!
+#results[num][result] << p.sort!
+            results[num][result] << p
+            
             results[num][result].uniq!
         end
     end
 
-    pp results
-
     possibilities = results[6].keys
 
+    #search each solution and only include results found in each array
+    #13,14,15 are found in all therefore these are possibilities for the
+    #sum of the leg
     results.each do |outer,outer_solutions|
         results.each do |out, solutions|
             next if outer == out
@@ -77,74 +80,98 @@ def main
             z = results[outer][num].collect do |sol| 
                 sol.insert(0,outer)
             end
-            puts "z=>#{z.inspect}"
-#z.insert(0, outer)
             totals[num] << z
-#results[outer][num].map! { |sol| sol.insert(0,outer) }
         end
     end
 
-    pp totals
 
-    #Since we have to start with the lowest number in the
-    #outer ring we must select the solutions for 6 first.
-    #This gives us 3 different solutions
-#all_perms = []
-#totals.each do |key, arr|
-        #start with the 6 array
-#path = []
-#0.upto(arr.size - 1).each do |num|
-#0.upto(arr[num].size-1).each do |ind|
-#path << arr[num][ind]
-#end
-#pp path
-#end
-#first.each do |f|
-##start with each 6
-#0.upto(
-#
-#end
-    
-
-#end
-    pp select(totals[14])
-
-=begin
-    totals.keys.each do |sol|
-        arr = totals[sol]
-        first = arr.first
-        a = arr.delete(first)
-        arr = arr.sort { |x,y| x[0] <=> y[0] }.reverse
-        a += arr.flatten
-        puts a.join('')
-    end
-
-    totals.each do |sum, arr|
-        #start with 6's possible solutions
-        for arr[0].each do |sol_6|
-            num = []
-            num << sol_6.flatten
-            1.upto(arr.size-1).each do |ind|
-                arr[ind]
-            end
-
-            
+    #format the arrays the way we want them, basically flatten them if they are length 1
+    totals.each do |key,arr|
+        arr.collect! do |values|
+            values.flatten! if values.size == 1
+            values
         end
+        totals[key] = arr
     end
-=end
+
+
+#    #We now have the possibilities, we just need to rearrange them into the correct order
+#    #Inner circle!
+#    totals.each do |key,arr| 
+#        0.upto(arr.size-1).each do |num|
+#            poss = []
+#            if arr[num][0].is_a?(Array) 
+#                arr[num].each do |z|
+#                    poss << z
+#                    poss << [z[0],z[2], z[1]]
+#                end
+#            else
+#                poss << arr[num] 
+#                poss << [arr[num][0],arr[num][2], arr[num][1]]
+#            end
+#            totals[key][num] = poss
+#        end
+#    end
+
+    #based on the possibilities for each sum (13,14,15), build all paths
+    results = []
+    totals.each do |key,value| 
+        results += select(value)
+    end
+
+    #filter the results for the correct inner circle
+    results.collect! do |result|
+        result if check_positions(result)
+    end
+
+    results.compact!
+
+    #now comb the results and build the strings we want
+    #this builds a string from the possible values
+    #and prints the maximum value of it
+    results.collect! do |result|
+        first = result.first
+        result.delete(first)
+        result.sort_by! {|a| a[0]}.reverse!
+        puts "sresults=>#{result.inspect}"
+        (first.join + result.join ).to_i
+    end
+
+    pp results.max
 end
 
-def select(array, path=[])
-    return array if path.size == 5
-    return array if array[0].is_a?(Fixnum)
+#This is the inner circle verifier.  You cannot have 
+#a number repeated in the same position in an array
+def check_positions(array)
+    array.each do |a|
+        array.each do |b|
+            next if a == b
+            return false if a[1] == b[1] || a[2] == b[2]
+        end
+    end
+
+    return true
+end
+
+#Recursively build a valid path of length 5
+def select(array, path=[], solutions=[])
+    if path.size == 5 #|| array.size == 0
+        solutions << path
+        return solutions 
+    end
 
     0.upto(array.size-1).each do |num|
         arr = array[num]
-        arr.each do |a|
-            path << a
-            select(array[(num..-1)], path)
+        if arr[0].is_a?(Array)
+            arr.each do |a|
+                select(array[(num+1..-1)], path + [a],solutions)
+            end
+        else
+            select(array[(num+1..-1)], path + [arr], solutions)
         end
     end
+
+    return solutions
 end
 
 if __FILE__ == $0
